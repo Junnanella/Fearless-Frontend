5e3b72d8c6c2e23a6bb59b124d4b9e60a8067c3b
Junella Caringal <junellacaringal@gmail.com>
2022-06-07 13:56:23 -0700

    Data to DOM completed. Working.

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -9,6 +9,20 @@ window.addEventListener("DOMContentLoaded", async () => {
       throw new Error("Response not ok");
     } else {
       const data = await response.json();
+
+      const conference = data.conferences[0];
+      const conferenceName = document.querySelector(".card-title");
+      conferenceName.innerHTML = conference.name;
+
+      const detailUrl = `http://localhost:8000${conference.href}`;
+      const detailResponse = await fetch(detailUrl);
+      if (detailResponse.ok) {
+        const details = await detailResponse.json();
+
+        const description = details.conference;
+        const conferenceDescription = document.querySelector(".card-text");
+        conferenceDescription.innerHTML = description.description;
+      }
     }
   } catch (e) {
     // Figure out what to do if an error is raised
