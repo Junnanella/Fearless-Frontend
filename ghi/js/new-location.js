e52240432060d52df5eef063e2dcc799254e141d
Junella Caringal <junellacaringal@gmail.com>
2022-06-08 12:12:50 -0700

    States now render in Catergory dropdown. Working.

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,1 +1,29 @@
-console.log("Hola");
+// add event listener for when the DOM loads
+window.addEventListener("DOMContentLoaded", async () => {
+  // declare a variable that will hold the URL for the API
+  const url = "http://localhost:8000/api/states/";
+  // fetch the URL, use await so that we get a response and not a promise
+  const response = await fetch(url);
+
+  if (response.ok) {
+    // get the data using json, don't forget to await
+    const data = await response.json();
+
+    // Get the select tag element by its id 'state'
+    const selectTag = document.querySelector("select#state");
+
+    // For each state in the states property of the data
+    for (const state of data.states) {
+      // Create an 'option' element
+      const option = document.createElement("option");
+      // Set the '.value' property of the option element to the
+      // state's abbreviation
+      option.value = state.abbreviation;
+      // Set the '.innerHTML' property of the option element to
+      // the state's name
+      option.innerHTML = state.name;
+      // Append the option element as a child of the select tag
+      selectTag.appendChild(option);
+    }
+  }
+});
