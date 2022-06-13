34f9c853146ce2f768f53a30e5bb7213938d5aa4
Junella Caringal <junellacaringal@gmail.com>
2022-06-13 12:36:58 -0700

    Created AttendeesList component and passed into App component. Working.

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,35 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./Nav";
 import React from "react";
+import AttendeesList from "./AttendeesList";
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <React.Fragment>
+    <>
       <Nav />
       <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Conference</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.attendees.map((attendee) => {
-              return (
-                <tr key={attendee.href}>
-                  <th>{attendee.name}</th>
-                  <th>{attendee.conference}</th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <AttendeesList attendees={props.attendees} />
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
